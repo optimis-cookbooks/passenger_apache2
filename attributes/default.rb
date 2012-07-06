28d4056f946205968013c9e9641de3189083118e
Mike Adolphs <mike@fooforge.com>
2012-07-06 17:54:42 +0200

    Temporary fix to get this beast running.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
+
 default[:passenger][:version]     = "3.0.13"
 default[:passenger][:max_pool_size] = "6"
 default[:passenger][:root_path]   = "#{languages[:ruby][:gems_dir]}/gems/passenger-#{passenger[:version]}"
@@ -5,7 +6,7 @@ default[:passenger][:module_path] = "#{passenger[:root_path]}/ext/apache2/mod_pa
 
 # rbenv integration
 default[:passenger][:rbenv][:enabled] = false
-default[:passenger][:rbenv][:version] = "ruby-1.9.3"
-default[:passenger][:rbenv][:binary] = "/usr/local/rbenv/versions/#{passenger[:rbenv][:version]}/bin/ruby"
-default[:passenger][:rbenv][:root_path] = "/usr/local/rbenv/versions/#{passenger[:rbenv][:version]}/lib/ruby/gems/1.9.1/gems/passenger-#{passenger[:version]}"
-default[:passenger][:rbenv][:module_path] = "#{passenger[:rbenv][:root_path]}/ext/apache2/mod_passenger.so}"
+default[:passenger][:rbenv][:version] = "ruby-1.9.2-p290-patched"
+default[:passenger][:rbenv][:binary] = "/usr/local/rbenv/versions/ruby-1.9.2-p290-patched/bin/ruby"
+default[:passenger][:rbenv][:root_path] = "/usr/local/rbenv/versions/ruby-1.9.2-p290-patched/lib/ruby/gems/1.9.1/gems/passenger-#{passenger[:version]}"
+default[:passenger][:rbenv][:module_path] = "#{passenger[:rbenv][:root_path]}/ext/apache2/mod_passenger.so"
